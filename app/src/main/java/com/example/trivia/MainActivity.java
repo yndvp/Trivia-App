32c7465b57ba244432b8957a709ba60810f70987
yndvp <58580623+yndvp@users.noreply.github.com>
2022-02-05 12:29:54 -0500

    build UI implement checking answer functionality implement next button functionality

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,7 +16,9 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.trivia.controller.AppController;
 import com.example.trivia.data.QuestionListAsyncResponse;
 import com.example.trivia.data.Repository;
+import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,17 +28,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+    List<Question> questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        List<Question> questions = new Repository().getQuestions(new QuestionListAsyncResponse() {
+        questions = new Repository().getQuestions(new QuestionListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
-                Log.d("Main", "onCreate: " + questionArrayList);
+                binding.questionTextview.setText((CharSequence) questionArrayList.get(currentQuestionIndex).getQuestion().toString());
+                updateCounter(questionArrayList);
             }
         });
+
+        binding.nextButton.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questions.size();
+            updateQuestion();
+        });
+        binding.trueButton.setOnClickListener(view -> {
+            checkCorrect(true);
+        });
+        binding.falseButton.setOnClickListener(view -> {
+            checkCorrect(false);
+        });
+    }
+
+    private void checkCorrect(boolean answerInput) {
+        boolean answer = questions.get(currentQuestionIndex).isAnswer();
+        int snackMessageId = 0;
+        if(answer == answerInput) {
+            snackMessageId = R.string.correct_answer;
+        } else {
+            snackMessageId = R.string.inccorect_answer;
+        }
+        Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT)
+                .show();
+    }
+
+    private void updateCounter(ArrayList<Question> questions) {
+        binding.textViewOutOf.setText(String.format(getString(R.string.text_formatted), currentQuestionIndex + 1, questions.size()));
+    }
+
+    private void updateQuestion() {
+        String question = questions.get(currentQuestionIndex).getQuestion();
+        binding.questionTextview.setText(question);
+        updateCounter((ArrayList<Question>) questions);
     }
 }
